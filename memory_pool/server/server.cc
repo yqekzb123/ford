36431725b189f9330ce7fe706848379b18848f0f
huangdund <1106412975@qq.com>
2023-12-04 13:41:31 +0800

    11

--- memory_pool/server/server.cc
+++ memory_pool/server/server.cc
@@ -78,16 +78,16 @@ void Server::LoadData(node_id_t machine_id,
   MemStoreReserveParam mem_store_reserve_param(hash_reserve_buffer, 0, hash_buffer + hash_buf_size);
   if (workload == "TATP") {
     tatp_server = new TATP();
-    tatp_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
+    // tatp_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
   } else if (workload == "SmallBank") {
     smallbank_server = new SmallBank();
     smallbank_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
   } else if (workload == "TPCC") {
     tpcc_server = new TPCC();
-    tpcc_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
+    // tpcc_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
   } else if (workload == "MICRO") {
     micro_server = new MICRO();
-    micro_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
+    // micro_server->LoadTable(machine_id, machine_num, &mem_store_alloc_param, &mem_store_reserve_param);
   }
   RDMA_LOG(INFO) << "Loading table successfully!";
 }
